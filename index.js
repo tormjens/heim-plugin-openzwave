89d56f8135e8c05a0ddcb7d78386da520b2c2956
Tor Morten Jensen <tormorten@anunatak.no>
2016-06-22 15:01:24 +0200

    add basics

---
+++ index.js
@@ -0,0 +1,37 @@
+var ZWave = require('openzwave-shared');
+
+module.exports = function(Heim, options) {
+	var zwave = new ZWave();
+
+	zwave.on('driver ready', function(homeid) {
+		Heim.Log('notice', 'OpenZWave: scanning homeid=0x'+homeid.toString(16)+'...')
+	});
+
+	zwave.on('driver failed', function() {
+	    Heim.Log('error', 'OpenZWave: Failed to start driver...');
+	    zwave.disconnect();
+	});
+
+	zwave.on('node ready', function(id, info) {
+		Heim.Devices.updateOrRegister({
+			provider: 'openzwave',
+			id: id,
+			name: info.name,
+			vendor: nodeinfo.manufacturer,
+			ready: true,
+			on: function() {
+				zwave.setNodeOn(id);
+			},
+			off: function() {
+				zwave.setNodeOn(id);
+			},
+			level: function(level) {
+				zwave.setNodeOn(id, level);
+			}
+		});
+	});
+
+	zwave.on('value changed', function(nodeid, comclass, value) {
+
+	});
+};
